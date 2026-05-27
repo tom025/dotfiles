fe282e3516a4f7e3f67e7fc77f9be60e389d8eda
Tom Brand <tom025@me.com>
2026-05-27 23:22:51 +0100

    Install and integrate bash language server into neovim

--- modules/nvim/config/lua/plugins/spec.lua
+++ modules/nvim/config/lua/plugins/spec.lua
@@ -1,8 +1,20 @@
 return {
-  "wincent/base16-nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme solarized-light]])
-  end,
+  {
+    "wincent/base16-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme solarized-light]])
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "bashls" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  }
 }
