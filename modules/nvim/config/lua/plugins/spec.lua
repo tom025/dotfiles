64a7448b2fb1af29cc3c9f063cf79622c8425205
Tom Brand <tom025@me.com>
2026-05-29 23:08:47 +0100

    Install neotest and the bash adapter

--- modules/nvim/config/lua/plugins/spec.lua
+++ modules/nvim/config/lua/plugins/spec.lua
@@ -97,4 +97,23 @@ return {
       end)
     end,
   },
+  {
+      "rcasia/neotest-bash"
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-bash")
+        }
+      })
+    end
+  },
 }
